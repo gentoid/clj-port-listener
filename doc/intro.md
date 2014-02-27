6116f2d128f458bd83b1ac5be03bc986ee577a42
Viktor Lazarev <taurus101v@gmail.com>
2014-02-28 01:27:20 +0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-port-listener
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_port_listener/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-port-listener.core-test
+  (:require [clojure.test :refer :all]
+            [clj-port-listener.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
